80493ab14d95936fa3f7e9d70ddaff53f10061a8
Angel Ortega <cs.angelortega@gmail.com>
2017-09-22 21:53:37 -0700

    Owners are queryable through LList

--- src/main/scala/OwnersList.scala
+++ src/main/scala/OwnersList.scala
@@ -1,19 +1,35 @@
+import scala.util.Success
+
+sealed trait OwnersList
+final case object EmptyOwnersList extends OwnersList
+final case class Owner(head: User, tail: OwnersList) extends OwnersList
+
+sealed trait User
+final case object Nil extends User
+final case class Username(owner: String) extends User
+
+
 /**
   * Define Recursive Data for Owners
   *
   * Owner(Username("Stu_Hood"), Owner(Username("Jack"), Owner(Username("jd"), NoneUser))
   * Owner(Username(Stu_Hood),Owner(Username(doe),Owner(Username(jd),NoneUser)))
   */
-sealed trait OwnersList
-final case object NoneUser extends OwnersList
-final case class Owner(head: User, tail: OwnersList) extends OwnersList
+object OwnerOps {
 
-sealed trait User
-final case class Username(owner: String) extends User
+  def getOwner(users: OwnersList, username: String): User = {
+    users match {
+      case Owner(head, tail) if Username(username) == head => head
+      case Owner(head, tail) => getOwner(tail, username)
+      case EmptyOwnersList => Nil
+    }
+  }
+}
 
 object OwnersList extends App {
-  val owners = Owner(Username("Stu_Hood"), Owner(Username("doe"), Owner(Username("jd"), NoneUser)))
-  println(owners)
+  val owners = Owner(Username("Stu_Hood"), Owner(Username("doe"), Owner(Username("jd"), EmptyOwnersList)))
+
+  println(OwnerOps.getOwner(owners,"jd"))
 }
 
 
