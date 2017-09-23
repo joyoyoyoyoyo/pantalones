ecbc91b93136d31e5d89c8db7fc85635340c4048
Angel Ortega <cs.angelortega@gmail.com>
2017-09-22 21:08:21 -0700

    Define recursive data structure

---
+++ src/main/scala/OwnersList.scala
@@ -0,0 +1,21 @@
+/**
+  * Define Recursive Data for Owners
+  *
+  * Owner(Username("Stu Hood"), Owner(Username("Jack"), Owner(Username("jd"), NoneUser))
+  * Owner(Username(Stu Hood),Owner(Username(doe),Owner(Username(jd),NoneUser)))
+  */
+sealed trait OwnersList
+final case object NoneUser extends OwnersList
+final case class Owner(head: User, tail: OwnersList) extends OwnersList
+
+sealed trait User
+final case class Username(owner: String) extends User
+
+object OwnersList extends App {
+  val owners = Owner(Username("Stu Hood"), Owner(Username("doe"), Owner(Username("jd"), NoneUser)))
+  println(owners)
+}
+
+
+
+
