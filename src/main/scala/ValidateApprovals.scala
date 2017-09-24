d50262b9a9b8210ffca97670b71f1c4a3a8d55f8
Angel Ortega <cs.angelortega@gmail.com>
2017-09-24 06:33:32 -0700

    Pushing stash

--- src/main/scala/ValidateApprovals.scala
+++ src/main/scala/ValidateApprovals.scala
@@ -4,17 +4,39 @@ import java.nio.file.{Files, Paths}
 
 import scala.collection.immutable.Queue
 import scala.io.Codec
-import java.util.concurrent._
+//import java.util.concurrent._
 
 import pantalones.common.{DefaultTaskScheduler, TaskScheduler}
 //import java.util.concurrent.RecursiveTask
 //import java.util.concurrent.ForkJoinPool
-//import scala.concurrent.
+import scala.collection.concurrent
 import scala.util.DynamicVariable
 
 object ValidateApprovals extends App {
 
-  def taskA( => )
+  // instantiate in memory model
+  val forkJoinPool = new
+  val cache = new concurrent.TrieMap[Int, String]()
+  val snapshot = cache.snapshot()
+
+  // do action in memory
+
+//  new AtomicReference[List[A]](Nil)
+  val depedencies
+//  cache {
+//    parse(freq, "johnny 121") match {
+//      case Success(matched,_) => println(matched)
+//      case Failure(msg,_) => println("FAILURE: " + msg)
+//      case Error(msg,_) => println("ERROR: " + msg)
+//    }
+//  }
+
+//  val snapshot =
+
+
+  def main(args: Array[String]): Unit = {
+
+  }
 
 }
 //object ValidateApprovals extends App {
